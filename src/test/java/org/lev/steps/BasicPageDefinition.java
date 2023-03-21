8fa16237f332117eaf0893b850cd4359015e31a8
Egor Lyubichev <EgorLyubichev@mail.ru>
2023-03-21 12:00:34 +0300

    Created scenario: check points of onliner.by apartments

--- src/test/java/org/lev/steps/BasicPageDefinition.java
+++ src/test/java/org/lev/steps/BasicPageDefinition.java
@@ -18,6 +18,7 @@ public class BasicPageDefinition {
     BasicPage basicPage = new BasicPage();
 
     private static final By AUTOBARAKHOLKA = By.xpath("//span[@class='b-main-navigation__text'][contains(text(), 'Автобарахолка')]/..");
+    private static final By HOUSES_AND_APARTMENTS = By.xpath("//span[@class='b-main-navigation__text'][contains(text(), 'Дома')]/..");
     private static final By CARS_UP_TO = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'Авто')]");
     private static final By MINSK_AUTOBARAKHOLKA = By.xpath("//a[@href='https://ab.onliner.by/city/minsk']/span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'Минск')]");
     private static final By GOMEL_AUTOBARAKHOLKA = By.xpath("//a[@href='https://ab.onliner.by/city/gomel']/span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'Гомель')]");
@@ -33,12 +34,24 @@ public class BasicPageDefinition {
     private static final By MAZDA_BRAND = By.xpath("//a[@href='https://ab.onliner.by/mazda']/span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'Mazda')]");
     private static final By MERCEDES_BRAND = By.xpath("//a[@href='https://ab.onliner.by/mercedes-benz']/span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'Mercedes-Benz')]");
 
+    private static final By ONE_ROOM = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), '1-комнатные')]");
+    private static final By TWO_ROOMS = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), '2-комнатные')]");
+    private static final By THREE_ROOMS = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), '3-комнатные')]");
+    private static final By FOUR_AND_MORE_ROOMS = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), '4+-комнатные')]");
+
+    private static final By UP_TO_30000$ = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'До 30 000 $')]");
+    private static final By FROM_30000_TO_80000 = By.xpath("//a[@href='https://r.onliner.by/pk/#price%5Bmin%5D=30000&price%5Bmax%5D=80000&currency=usd']");
+    private static final By FROM_80000$ = By.xpath("//span[@class='b-main-navigation__dropdown-advert-sign'][contains(text(), 'От 80 000 $')]");
+
 
     @When("hover the cursor over {string}")
     public void hoverTheCursorTheElement(String string) {
         switch (string) {
             case "Автобарахолка":
                 basicPage.activateTheElement(AUTOBARAKHOLKA);
+                break;
+            case "Дома и квартиры":
+                basicPage.activateTheElement(HOUSES_AND_APARTMENTS);
                 break;
             default:
                 System.err.println("ERROR: Case was not found to hover over '" + string + "'!");
@@ -75,6 +88,20 @@ public class BasicPageDefinition {
                         ()-> assertTrue($(FORD_BRAND).exists()),
                         ()-> assertTrue($(MAZDA_BRAND).exists()),
                         ()-> assertTrue($(MERCEDES_BRAND).exists())
+                );
+                break;
+            case "rooms amount":
+                assertAll("test rooms amount",
+                        ()-> assertTrue($(ONE_ROOM).exists()),
+                        ()-> assertTrue($(TWO_ROOMS).exists()),
+                        ()-> assertTrue($(THREE_ROOMS).exists()),
+                        ()-> assertTrue($(FOUR_AND_MORE_ROOMS).exists())
+                );
+            case "apartment price":
+                assertAll("test price apartment",
+                        ()-> assertTrue($(UP_TO_30000$).exists()),
+                        ()-> assertTrue($(FROM_30000_TO_80000).exists()),
+                        ()-> assertTrue($(FROM_80000$).exists())
                 );
                 break;
             default:
--- src/test/resources/features/onliner_auto.feature
+++ src/test/resources/features/onliner_auto.feature
@@ -5,3 +5,9 @@ Feature: Onliner Auto Window
     Then browse fields "Авто до ..."
     Then browse fields "cities"
     Then browse fields "auto brands"
+
+  Scenario: check points of onliner.by apartments
+    When hover the cursor over "Дома и квартиры"
+    Then browse fields "cities"
+    Then browse fields "rooms amount"
+    Then browse fields "apartment price"
